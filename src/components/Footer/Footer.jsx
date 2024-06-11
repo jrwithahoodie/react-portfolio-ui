0296025756196613eb039804d78b8169f754ab65
Jr5Dev11 <jrgxllego@proton.me>
2024-06-11 21:12:50 +0200

    [ADD] - footer social media working and styles for mobiles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -33,13 +33,13 @@ const Footer = () => {
                 Hello, I'm José Ramón, a junior developer with 2 years of experience in web and cross-platform applications. Skilled in various front-end and back-end technologies, I focus on creating efficient and robust solutions. With a Higher Degree in Cross-Platform Application Development and a certification in OKR and Product Owner from "TheUncoding," I am always eager to learn and take on new challenges. Let's connect!
                 </p>
                 <div className="social">
-                    <a href="github.com/jrwithahoodie" className="social-button github">
+                    <a href="https://www.github.com/jrwithahoodie" className="social-button github">
                         <FaGithub className="cf-icon-svg"></FaGithub>
                     </a>
-                    <a href="github.com/jrwithahoodie" className="social-button linkedin">
+                    <a href="https://www.linkedin.com/in/jos%C3%A9-ram%C3%B3n-gallego-v%C3%A9lez-92b738242?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" className="social-button linkedin">
                         <FaLinkedin className="cf-icon-svg"></FaLinkedin>
                     </a>
-                    <a href="github.com/jrwithahoodie" className="social-button instagram">
+                    <a href="https://www.instagram.com/jr.withahoodie" className="social-button instagram">
                         <FaInstagram className="cf-icon-svg"></FaInstagram>
                     </a>
                 </div>
